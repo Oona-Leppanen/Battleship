8ca52847b87efb365007ac1f317b5a2668884fe2
Oona <oklepp@utu.fi>
2021-03-08 17:53:35 +0200

    I did some fixis to Valiruutu's size

--- src/battleship/ValiruutuController.java
+++ src/battleship/ValiruutuController.java
@@ -2,7 +2,9 @@ package battleship;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class ValiruutuController {
 
@@ -11,7 +13,9 @@ public class ValiruutuController {
 
     @FXML
     void Continue(ActionEvent event) {
-
+    	Node node = (Node) event.getSource(); //Tallennetaan nappi muuttujaan node
+    	Stage stage = (Stage) node.getScene().getWindow(); //Haetaan napin scen ja Scenen ikkuna eli Stage -> tallennetaan stage
+    	stage.close();
     }
 
 }
