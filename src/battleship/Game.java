b7d113c6fb5597ddff639214fc9fd725fdc4f7f8
vamivir <vamivir@utu.fi>
2021-03-31 19:47:24 +0300

    Cleaning up the game logic files and adding comments.

--- src/battleship/Game.java
+++ src/battleship/Game.java
@@ -16,8 +16,8 @@ public class Game {
     boolean board1set = false;
 
     public Game (String player1, String player2, int sizeX, int sizeY) {
-        playerBoard1 = new Board(sizeX, sizeY, Player.P1);
-        playerBoard2 = new Board(sizeX, sizeY, Player.P2);
+        playerBoard1 = new Board(sizeX, sizeY);
+        playerBoard2 = new Board(sizeX, sizeY);
         this.sizeX=sizeX;
         this.sizeY=sizeY;
         this.player1=player1;
--- src/battleship/Ship.java
+++ src/battleship/Ship.java
@@ -1,5 +1,9 @@
 package battleship;
 
+/* 
+ * Ship class handles the ship logic on the logic side of the program. 
+ * It has attributes for size, hits, verticality and coordinates.
+ */
 
 public class Ship {
 	int size;
@@ -14,6 +18,10 @@ public class Ship {
 		this.vertical = vertical;
 	}
 
+/*
+ * Checks if the ship is still alive or if it has been destroyed. Returns true if ship is alive.
+ */
+	
 	boolean isAlive() {
 		if (hits == size) {
 			alive = false;
@@ -27,10 +35,19 @@ public class Ship {
 		this.coordY =  y;
 	}
 
+/*
+ * Registers a hit to the ship
+ */
+	
 	void gotHit() {
 		this.hits += 1;
 	}
 
+/*
+ * Returns the ships' coordinates from the board. Order defines which coordinate of the ship is given.
+ * This information is used in other classes to loop through every coordinate the ship.
+ */
+	
 	public int[] onBoard(int order) {
 		if (vertical) {
 			int [] coordinates = new int[2];
@@ -42,7 +59,6 @@ public class Ship {
 			int [] coordinates = new int[2];
 			coordinates[0] = coordX+order;
 			coordinates[1] = coordY;
-
 			return coordinates;
 		}
 	}
